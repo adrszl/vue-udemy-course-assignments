7bf21b8f604d311ea9c950e295120cc821fa6596
Adrian Szlegel <adrian.szlegel@hotmail.com>
2021-06-02 17:21:59 +0200

    assignment 6 finished

---
+++ Assignment 6/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <active-users :username="user.name" :userage="user.age"></active-users>
+    <user-data @set-data="setUserData"></user-data>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Adrian",
+        age: "25"
+      }
+    }
+  },
+  methods: {
+    setUserData(name, age) {
+      this.user = {
+        name: name,
+        age: age
+      };
+    }
+  }
+}
+</script>
---
+++ Assignment 6/src/components/UserData.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <form @submit.prevent="submitData">
+      <label for="user-name">Name</label>
+      <input name="user-name" v-model="enteredName" />
+      <label for="user-age">Age</label>
+      <input name="user-age" v-model="enteredAge" />
+      <button>Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['set-data'],
+  data() {
+    return {
+      enteredName: '',
+      enteredAge: ''
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit('set-data', this.enteredName, this.enteredAge);  
+    }
+  }
+}
+</script>
